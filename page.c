690a68169d073e1c4c32d65cbb09e977c704e231
dinav2 <wealthydina@icloud.com>
2026-04-18 02:53:56 -0600

    Add page module with layout, structs, and record operations

---
+++ page.c
@@ -0,0 +1,57 @@
+#include <string.h>
+
+#include "page.h"
+
+static PageHeader *page_header(Page *pp) { return (PageHeader *)pp->data; }
+static DatabaseHeader *db_header(Page *pp) {
+  return (DatabaseHeader *)(pp->data + PAGE_HEADER_SIZE);
+}
+
+static const PageHeader *page_header_const(const Page *pp) {
+  return (PageHeader *)pp->data;
+}
+
+u32 get_page_id(const Page *pp) { return page_header_const(pp)->page_id; }
+
+u32 get_page_free(const Page *pp) {
+  return PAGE_SIZE - page_header_const(pp)->free_offset;
+}
+
+int page_init(Page *pp, u32 page_id, u8 page_type) {
+  // Initialize buffer with all 0s
+  memset(pp->data, 0, PAGE_SIZE);
+
+  PageHeader *h = page_header(pp);
+
+  // write page header
+  h->page_id = page_id;
+  h->free_offset = PAGE_HEADER_SIZE;
+  h->page_type = page_type;
+
+  return 0;
+}
+
+int header_page_init(Page *pp, u32 page_id) {
+  page_init(pp, page_id, PAGE_TYPE_HEADER);
+  DatabaseHeader *dh = db_header(pp);
+
+  dh->hdr_page_size = PAGE_SIZE;
+  dh->hdr_version = 1;
+
+  return 0;
+}
+
+int page_add_record(Page *pp, const void *record, u32 length) {
+  PageHeader *header = page_header(pp);
+  u32 free_bytes = PAGE_SIZE - header->free_offset;
+
+  if (length > free_bytes) {
+    // create/look for a new page
+    return 1;
+  } else {
+    memcpy(pp->data + header->free_offset, record, length);
+    header->free_offset += length;
+  }
+
+  return 0;
+}
---
+++ page.h
@@ -0,0 +1,64 @@
+#pragma once
+
+/* Page Layout (4096 bytes)
+ *
+ * +--------+--------------+------+
+ * | Offset | Field        | Size |
+ * +--------+--------------+------+
+ * |      0 | page_id      |    4 |
+ * |      4 | free_offset  |    4 |
+ * |      8 | record_count |    4 |
+ * |      12 | (reserved)   |  116 |
+ * +--------+--------------+------+
+ * |    128 | payload      | 3968 |
+ * +--------+--------------+------+
+ */
+
+#define PAGE_SIZE 4096
+#define PAGE_HEADER_SIZE 128
+
+// Page Header
+#define PAGE_ID_OFFSET 0
+#define PAGE_FREE_OFFSET 4
+#define PAGE_RECORD_COUNT_OFFSET 8
+#define PAGE_TYPE_OFFSET 12
+
+// Database Header
+
+// Page Types
+#define PAGE_TYPE_HEADER 0x01
+#define PAGE_TYPE_DATA 0x02
+#define PAGE_TYPE_CATALOG 0x03
+#define PAGE_TYPE_SCHEMA 0x04
+
+#include "types.h"
+
+typedef struct {
+  u16 hdr_version;
+  u16 hdr_page_size;
+} DatabaseHeader;
+
+typedef struct {
+  u32 page_id;
+  u32 free_offset;
+  u32 record_count;
+  u32 next_page_id;
+  u8 page_type;
+} PageHeader;
+
+typedef struct {
+  u8 data[PAGE_SIZE];
+} Page;
+
+// temporal record definition
+typedef struct {
+  int id;
+  int x;
+  int y;
+} Record;
+
+u32 get_page_id(const Page *pp);
+u32 get_page_free(const Page *pp);
+int page_add_record(Page *pp, const void *record, u32 length);
+int page_init(Page *pp, u32 page_count, u8 page_type);
+int header_page_init(Page *pp, u32 page_id);
