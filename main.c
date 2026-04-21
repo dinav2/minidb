cb4c09c692167de9045a149197f745024aed58db
dinav2 <wealthydina@icloud.com>
2026-04-20 20:14:52 -0600

    Update test program to exercise scan and delete

--- main.c
+++ main.c
@@ -7,15 +7,37 @@
 
 int main(void) {
   Database db;
-  db_open(&db, "test.db");
+  db_open(&db, "database.db");
   printf("Number of pages: %u\n", db.pager.page_count);
 
   Column c1 = {.type = 0, .size = 4, .name = "column1"};
   db_create_table(&db, "Table1", &c1, 1);
 
   i32 buf = 156;
-  for (u32 i = 0; i < 10000; i++) {
+  for (u32 i = 0; i < 10; i++) {
     db_insert_row(&db, "Table1", &buf, sizeof(i32));
+    buf += 10;
+  }
+
+  Cursor c;
+  db_scan_table(&db, "Table1", &c);
+
+  i32 row;
+  u32 count = 0;
+  while (db_scan_next(&db, &c, &row) == SCAN_OK) {
+    printf("%u\n", row);
+    if (count % 2 == 0) {
+      db_delete_row(&db, &c);
+    }
+
+    count++;
+  }
+
+  printf("\n\n");
+  db_scan_table(&db, "Table1", &c);
+
+  while (db_scan_next(&db, &c, &row) == SCAN_OK) {
+    printf("%u\n", row);
   }
 
   db_close(&db);
